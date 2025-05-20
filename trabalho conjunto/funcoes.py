023b80476be24ac193cdbe2671f10b1967ffe33b
Satiro07 <jardelsatiro57@gmail.com>
2025-05-20 19:32:49 -0300

    Update funcoes.py

---
+++ trabalho conjunto/funcoes.py
@@ -0,0 +1,34 @@
+lista_conjuntos = []
+
+def menu():
+    print('1. Criar conjunto')
+    print('2. Adicionar elemento (Em um conjunto ja existente!)')
+    print('3.Remover elemento de um conjunto')
+    print('4. Mostrar conjuntos')
+    print('5. Apagar conjunto')
+    print('6. União de conjuntos')
+    print('7. Intersecção de conjuntos')
+
+def criar_conjunto(conjuntos):
+    nome_conjunto = input('Digite o nome do conjunto que deseja criar: ')
+    elementos = []
+    if nome_conjunto not in conjuntos:
+        opcao = 's'
+        while opcao != 'n':
+            elemento = int(input('Digite um elemento'))
+            if elemento not in elementos:
+                elementos.append(elemento)
+                print(f'Elemento "{elemento}" adicionado com sucesso!')
+            else:
+                print(f'Elemento "{elemento}" já está no conjunto!')
+            opcao = input('Deseja adicionar mais elementos? [s/n] ').lower()
+        return conjuntos.append(nome_conjunto, elementos)
+    else:
+        print(f'O nome "{nome_conjunto}" já está em uso!')
+
+
+while True:
+    menu()
+    escolha = input('Escolha uma opção: ')
+    if escolha == '1':
+        funcao = criar_conjunto(lista_conjuntos)
