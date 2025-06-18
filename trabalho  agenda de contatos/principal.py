ebdfc8d824a62dad2aea41be5104abbba0c16255
Satiro07 <jardelsatiro57@gmail.com>
2025-06-18 19:52:10 -0300

    Update principal.py

--- trabalho  agenda de contatos/principal.py
+++ trabalho  agenda de contatos/principal.py
@@ -1,50 +1,54 @@
 import json
 
 def menu():
-    print('-=-' * 20)
-    print('--- AGENDA DE CONTATOS ---')
+    print()
+    print('-=-' * 15)
+    print(f'{"-"*12} AGENDA DE CONTATOS {"-"*12}')
     print('1. Adicionar contato')
     print('2. Buscar contato')
     print('3. Editar contato')
     print('4. Remover contato')
     print('5. Sair')
     opcao = input('Escolha uma opção: [1,2,3,4,5] ')
-    print('-=-' * 20)
+    print('-=-' * 15)
     return opcao
 
 def menu_edicao():
-    print('-=-' * 20)
+    print()
+    print('-=-' * 15)
     print('O que deseja editar? ')
     print('1. Nome')
     print('2. Número')
     print('3. Email')
     opcao = input('Escolha uma opção: [1,2,3] ').lower()
-    print('-=-' * 20)
-    print()
+    print('-=-' * 15)
+    print(' - '*15)
     return opcao
 
 def mostrar_contatos():
-    print('-=-' * 20)
+    print()
+    print('-=-' * 15)
     print('Usuários adicionados: ')
     cont = 1
     for nome_user in agenda.keys():
         print(f'{cont}- {nome_user}')
         cont += 1
-    print('-=-' * 20)
-    print()
+    print('-=-' * 15)
+    print(' - '*15)
 
 def adicionar_contato(agenda, usuario, nome, celular, email):
-    print('-=-' * 20)
+    print()
+    print('-=-' * 15)
     agenda[usuario] = [nome, celular, email]
     salvar_agenda(agenda)
     print('Contato salvo com sucesso!')
-    print('-=-' * 20)
-    print()
+    print('-=-' * 15)
 
 def buscar_contato(agenda, usuario):
-    print('-=-' * 20)
+    print()
+    print('-=-' * 15)
     if usuario in agenda.keys():
-        print()
+        print(' - '*15)
         print(f'Informações do(a) usuário(a) "{usuario}": ')
         print(f'Nome: {agenda[usuario][0]}')
         print(f'Número: {agenda[usuario][1]}')
@@ -52,11 +56,12 @@ def buscar_contato(agenda, usuario):
 
     else:
         print(f'Usuário "{usuario}" não encontrado!')
-    print('-=-' * 20)
-    print()
+    print('-=-' * 15)
+    print(' - '*15)
 
 def editar_contato(agenda, usuario, campo, novo_valor):
-    print('-=-' * 20)
+    print()
+    print('-=-' * 15)
     if campo == '1':
         nome_campo = 'nome'
 
@@ -74,14 +79,15 @@ def editar_contato(agenda, usuario, campo, novo_valor):
 
         if usuario in agenda.keys():
             agenda[usuario][2] = novo_valor
-
+    print(' - '*15)
     print(f'Novo {nome_campo} salvo com sucesso!')
     salvar_agenda(agenda)
-    print('-=-' * 20)
-    print()
+    print('-=-' * 15)
+    print(' - '*15)
 
 def remover_contato(agenda, usuario):
-    print('-=-' * 20)
+    print()
+    print('-=-' * 15)
     if usuario in agenda.keys():
         del agenda[usuario]
         print(f'Usuário "{usuario}" removido com sucesso!')
@@ -89,13 +95,13 @@ def remover_contato(agenda, usuario):
 
     else:
         print(f'Usuário "{usuario}" não encontrado!')
-    print('-=-' * 20)
-    print()
+    print(' - '*15)
+    print('-=-' * 15)
 
 def salvar_agenda(agenda):
 
     with open('contatos.json', 'w') as f:
-        json.dump(agenda, f, indent=4)
+        json.dump(agenda, f, ensure_ascii=False, indent=4)
 
 def carregar_agenda():
     if 'contatos.json':
@@ -126,7 +132,7 @@ while True:
                 for num in numero:
                     contagem += 1
                     if numero[5] == '-':
-                        if num in '0123456789':
+                        if num in '0123156789':
                             if contagem == 10:
                                 verificacao_geral = True
                         continue
@@ -189,7 +195,7 @@ while True:
                         for num in novo_numero:
                             contagem += 1
 
-                            if num in '0123456789':
+                            if num in '0123156789':
 
                                 if contagem == 10:
                                     verificacao_geral = True
@@ -244,12 +250,17 @@ while True:
 
         else:
             print('Adicione pelo menos um contato para fazer uma busca!')
+            print()
         
 
     elif opcao == '5':
+        print(' - '*15)
         print('Saindo do programa...')
+        print(' - '*15)
+        print('-=-' * 15)
         break
 
     else:
         print('Escolha uma opção válida!')
         print('Digite "1" ou "2" ou "3" ou "4" ou "5"!')
+        print(' - '*15)
