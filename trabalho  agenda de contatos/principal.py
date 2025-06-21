039582f60170094cacd0f4055968411194ba2268
Satiro07 <jardelsatiro57@gmail.com>
2025-06-21 09:44:58 -0300

    Update principal.py

--- trabalho  agenda de contatos/principal.py
+++ trabalho  agenda de contatos/principal.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+pribidos_no_email = ["""() <> [] ; : , \ / " ' ` ~ = + * & % $ # ! { } | ? ^"""]
+
 def menu():
     print()
     print('-=-' * 15)
@@ -140,18 +142,23 @@ while True:
 
                     if verificacao_geral == True:
                         email = input('Digite o email do contato: ').lower().strip()
-                        cont_arroba = email.count('@')
-
-                        pontos_seguidos = 0
-                        for i in range(len(email)+1):
-                            if i == len(email)-1:
-                                break
-                            if email[i] == email[i+1] and email[i] == '.':
-                                pontos_seguidos += 1
+                        cont_arroba = email.count('@') #conta quantos '@' o usuário escreveu na variável 'email'
                         verificacao = False
-                        if '@' in email:
-                            posicao_arroba = email.index('@')
-                            if cont_arroba == 1 and email[0] != '@' and email[0] != '.' and email[-1] != '@' and email[-1] != '.' and pontos_seguidos == 0 and email[posicao_arroba-1] != '.' and email[posicao_arroba+1] != '.':
+                        verificacao_proibidos = False
+                        for string in pribidos_no_email:
+                            if string in email:
+                                verificacao_proibidos = True
+                                break
+                        if cont_arroba == 1 and verificacao_proibidos == False:
+                            pontos_seguidos = 0 # variável para ajudar na verificação
+                            for i in range(len(email)+1):
+                                if i == len(email)-1: # condição para o codigo não quebar
+                                    break
+                                if email[i] == email[i+1] and email[i] == '.': # se uma string em uma pocição for igual ao do lado, e a string verificada for um ponto
+                                    pontos_seguidos += 1 
+
+                            posicao_arroba = email.index('@') # posição de onde está o '@'
+                            if email[0] != '@' and email[0] != '.' and email[-1] != '@' and email[-1] != '.' and pontos_seguidos == 0 and email[posicao_arroba-1] != '.' and email[posicao_arroba+1] != '.': # se não começar com "@" nem "." e não terminar com "@" nem "." e não tiver pontos seguidos e antes e dps do arroba não for um "."
                                 if len(email[posicao_arroba:]) >= 4:
                                     sub_email = email[posicao_arroba:]
                                     if '.' in sub_email:
