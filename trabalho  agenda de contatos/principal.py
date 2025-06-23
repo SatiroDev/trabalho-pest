f4bc9cd7f70d628287b1d4022f03adede8d130d2
Satiro07 <jardelsatiro57@gmail.com>
2025-06-22 22:09:20 -0300

    Update principal.py

--- trabalho  agenda de contatos/principal.py
+++ trabalho  agenda de contatos/principal.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-pribidos_no_email = ["""() <> [] ; : , \ / " ' ` ~ = + * & % $ # ! { } | ? ^"""]
+pribidos_no_email = ["""() <> [] ; : , \ / " ' ` ~ = + * & % $ # ! { } | ? ^ ç á ã â à é ê è ó ò õ ô í ì î ú ù û"""] # lista com itens que não pode ter no email
 
 def menu():
     print()
@@ -68,7 +68,22 @@ def validar_email(email):
                             if '.' not in sub_email[-2:]: # se um '.' não estiver nem na penúltima e nem última posição 
                                 verificacao = True
                                                             
-    return verificacao
+    return verificacao # retorna True ou False
+
+def validar_numero(numero):
+    contagem = 0
+    verificacao_numero = False
+    if len(numero) == 10:
+        for num in numero:
+            if numero[5] == '-':
+                if num in '0123456789':
+                    contagem += 1
+                    if contagem == 9:
+                        verificacao_numero = True
+                continue
+            else:
+                break
+    return verificacao_numero
 
 def mostrar_contatos():
     print()
@@ -169,31 +184,18 @@ while True:
             if usuario[0] == '@':
                 nome = input('Digite o nome do contato: ').title().strip()
                 numero = input('Digite o número: (ex: 99999-9999) ').strip()
-                contagem = 0
+                if validar_numero(numero):
+                    verificacao_geral = True
 
-                if len(numero) == 10:
+                if verificacao_geral:
+                    email = input('Digite o email do contato: ').lower().strip()
+                    print(validar_email(email))
+                    if validar_email(email):
+                        adicionar_contato(agenda, usuario, nome, numero, email)
 
-                    for num in numero:
-                        if numero[5] == '-':
-                            if num in '0123456789':
-                                contagem += 1
-                                if contagem == 9:
-                                    verificacao_geral = True
-                            continue
-                        else:
-                            break
-
-                    if verificacao_geral == True:
-                        email = input('Digite o email do contato: ').lower().strip()
-                        print(validar_email(email))
-                        if validar_email(email):
-                            adicionar_contato(agenda, usuario, nome, numero, email)
-
-                        else:
-                            print('Email inválido!')
-                    
-                if verificacao_geral == False:
-
+                    else:
+                        print('Email inválido!')
+                else:
                     print('Erro! O número tem que seguir esse padrão -> 99999-9999')
 
             else:
@@ -227,90 +229,21 @@ while True:
                     editar_contato(agenda, usuario, opcao_edicao, novo_nome)
 
                 elif opcao_edicao == '2':
+
                     novo_numero = input('Digite o novo número: (ex: 99999-9999) ')
-                    verificacao_geral = False
-                    verificacao = True
-                    verificacao_traco = False
-                    contagem = 0
-
-                    if len(novo_numero) == 10:
-
-                        for num in novo_numero:
-
-                            if num in '0123156789':
-                                contagem += 1
-                                if contagem == 9:
-                                    verificacao_geral = True
-                                continue
-
-                            elif num == novo_numero[5] and num == '-':
-                                verificacao_traco = True
-                                continue
-
-                            else:
-
-                                if verificacao_traco == False:
-                                    print('Precisa ter o "-"!')
-                                    print('O número tem que seguir esse padrão -> 99999-9999')
-                                    break
-
-                                else:
-                                    print('Você tem que digitar números!')
-                                    verificacao = False
-                                    break
-
-                        if verificacao_geral == True:
-                            editar_contato(agenda, usuario, opcao_edicao, novo_numero)         
+                    
+                    if validar_numero(novo_numero):
+                        editar_contato(agenda, usuario, opcao_edicao, novo_numero)         
 
                     else:
                         print('Erro! O número tem que seguir esse padrão -> 99999-9999')
 
                 elif opcao_edicao == '3':
                     novo_email = input('Digite o novo email do contato: ').lower().strip()
-                    cont_arroba = email.count('@') #conta quantos '@' o usuário escreveu na variável 'email'
-                    verificacao = False
-                    verificacao_proibidos = False
-  
-                    for caracter in pribidos_no_email:
-                        if caracter in email: # se um caractere proibido estiver no email que o usuário digitou
-                            verificacao_proibidos = True
-                            break
-                    if cont_arroba == 1 and verificacao_proibidos == False: # se o email tiver so um '@' e não tiver nenhum caractere  proibido
-                        pontos_seguidos = 0 # variável para ajudar na verificação
-                        for i in range(len(email)+1):
-                            if i == len(email)-1: # condição para o codigo não quebar
-                                break
-                            if email[i] == email[i+1] and email[i] == '.': # se uma string em uma pocição for igual ao do lado, e a string verificada for um ponto
-                                pontos_seguidos += 1 
-                                                    
-                        posicao_arroba = email.index('@') # posição de onde está o '@'
-                        sub_email_antes_do_arroba = email[:posicao_arroba] # sub_email, do começo até antes do "@"
-                            
-                        verificacao_email = True
-                        if len(sub_email_antes_do_arroba) >= 3:
-                            if sub_email_antes_do_arroba[-2] == '.' and sub_email_antes_do_arroba[-1] == '-' or sub_email_antes_do_arroba[-1] == '_' and sub_email_antes_do_arroba[-2] == '.':
-                                verificacao_email = False
-                        email_invertido = email[::-1]
-                        if '.' in email_invertido:
-                            posicao_ponto = email_invertido.index('.')
-                            verificacao_numero = True
-                            for i in email_invertido[:posicao_ponto]: # verificar se tem número na extensão
-                                if i in '1234567890':
-                                    verificacao_numero = False
-                                    break
-                        if email_invertido[posicao_ponto+1] != '-' and email_invertido[posicao_ponto-1] != '-' and verificacao_numero == True:
-                            if verificacao_email == True and email[0] != '@' and email[0] != '.' and email[-1] != '@' and email[-1] != '.' and pontos_seguidos == 0 and email[posicao_arroba-1] != '.' and email[posicao_arroba+1] != '.' and email[posicao_arroba+1] != '-' and email[posicao_arroba-1] != '-'and '_' not in email[posicao_arroba:]: # se não começar com "@" nem "." e não terminar com "@" nem "." e não tiver pontos seguidos e antes e dps do arroba não for um "." e se não tem "_" depois do "@"
-                                if email[0] != "-" and email[0] != "_" and email[-1] != "-" and email[-1] != "_":
-                                    if len(email[posicao_arroba:]) >= 4: # se o tamanho da 'sub_string' começando de onde está o '@' for maior que 4
-                                        sub_email = email[posicao_arroba:]
-                                        if '.' in sub_email: # se tiver '.' no "sub_email"
-                                            if '.' not in sub_email[-2:]: # se um '.' não estiver nem na penúltima e nem última posição 
-                                                print('Validooo!!')
-                                                verificacao = True
-                                                            
-                        if verificacao == False:
-                            print('Email inválido!')
-
+                    if validar_email(novo_email):
+                        editar_contato(agenda, usuario, opcao_edicao, novo_email)
+                    else:
+                        print('Email inválido!')
                 else:
                     verificacao = False
                     print('Opção inválida!')
