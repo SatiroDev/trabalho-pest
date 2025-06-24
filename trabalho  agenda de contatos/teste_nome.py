a8f0baac234518458a3f70f5275cd01d1f90f372
Satiro07 <jardelsatiro57@gmail.com>
2025-06-24 20:14:54 -0300

    Update teste_nome.py

--- trabalho  agenda de contatos/teste_nome.py
+++ trabalho  agenda de contatos/teste_nome.py
@@ -1,16 +1,22 @@
 
 
 # for nome in nomes:
-nome = input('Digite o nome do contato: ').title().strip()
-verificacao_nome = False
-for i in range(len(nome)):
-    if nome[i] in '0123456789':
-        break
-    else:
-        if i == len(nome)-1:
-            verificacao_nome = True
-print(nome)
-if verificacao_nome:
-    print('certooo')
-else:
-    print('Erro')
+# nome = input('Digite o nome do contato: ').title().strip()
+# verificacao_nome = False
+# for i in range(len(nome)):
+#     if nome[i] in '0123456789':
+#         break
+#     else:
+#         if i == len(nome)-1:
+#             verificacao_nome = True
+# print(nome)
+# if verificacao_nome:
+#     print('certooo')
+# else:
+#     print('Erro')
+
+titulo = " CADASTRO DE USUÁRIOS "
+largura = len(titulo) + 4
+print('+' + '-' * (largura - 2) + '+')
+print(f'| {titulo} |')
+print('+' + '-' * (largura - 2) + '+')
